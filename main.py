d1991bc41ed8d91e5e52e094c87f8b9a83142c7b
masayuki <masa@masayuki-no-MacBook-Air.local>
2018-03-03 18:11:06 +0900

    seika

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
 # -*- coding: utf-8 -*-
-import pygame
+import pygame,math
 from pygame.locals import *
 import sys
 
-SCREEN_SIZE = (800,600) # スクリーンサイズ（px指定）
-#Pygame初期化
-pygame.init()
-#SCREEN_SIZEの画面作成
-screen = pygame.display.set_mode(SCREEN_SIZE)
-#タイトルバーの文字セット
-pygame.display.set_caption("初めてのシューティング")
+START, PLAY, GAMEOVER = (0, 1, 2) #ゲーム状態
+SCR_RECT = Rect(0,0,800,600) # スクリーンサイズ（px指定）
 
-#ゲームイベントループ
-while True:
-    screen.fill((0,0,0)) #画面を真っ青で塗りつぶす
-    pygame.display.update() #画面を更新
-    #イベント処理
-    for event in pygame.event.get():
-        if event.type == QUIT: # 終了イベント
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                sys.exit()
-            pressed_keys = pygame.key.get_pressed()
-            MyPC.move(pressed_keys)
+class Game:
+    def __init__(self):
+        #各種読み込み
+        pygame.init()
+        screen = pygame.display.set_mode(SCR_RECT.size)
+        pygame.display.set_caption("初めてのシューティング")
+        #ゲームオブジェクトを初期化
+        self.init_game()
+        #メインループ開始
+        clock = pygame.time.Clock()
+        while True:
+            clock.tick(60)
+            self.update()
+            self.draw(screen)
+            pygame.display.update()
+            print('yeah')
+            #self.key_handler()
+
+
+    def init_game(self):
+        #ゲームオブジェクトを初期化
+
+        #ゲーム状態
+        self.game_state = START
+
+
+    def update(self):
+        pass
+
+
+
+    def draw(self,screen):
+        #描画
+        screen.fill((0, 0, 0))
+        if self.game_state == START:
+            sysfont = pygame.font.SysFont(None, 80)
+            sys = sysfont.render("PUSH SPACE TO START",False,(255,0,0))
+            screen.blit(sys, ((SCR_RECT.width-sys.get_width())/2,270))
+
+        if self.game_state == PLAY:
+            self.all_sprite.draw(screen)
+
+        if self.game_state == GAMEOVER:
+            sysfont = pygame.font.SysFont(None, 80)
+            sys = sysfont.render("GAMEOVER",False,(255,0,0))
+            screen.blit(sys, ((SCR_RECT.width-sys.get_width())/2,270))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    Game()
